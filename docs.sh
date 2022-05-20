6a8e7c2b8d8ecd1f78a084171209b0016e1c3b63
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-05-20 12:47:16 -0500

    generate docs

--- docs.sh
+++ docs.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 rm -rf ./docs/*.html
-sed -i.bak '1,2d' ./src/amps/__init__.py
+sed -i '1,2d' ./src/amps/__init__.py
 pdoc3 --html ./src/amps --output-dir ./docs --force --template-dir ./templates
 mv ./docs/amps/* ./docs
 rm -rf ./docs/amps
 echo 'from erlport.erlang import cast, call\nfrom erlport.erlterms import Atom' | cat - ./src/amps/__init__.py > temp && mv temp ./src/amps/__init__.py
-rm ./src/amps/*.bak
 
