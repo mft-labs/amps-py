6dc92f6678bd50f4aeeb76bb115fd9264d57ab00
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-05-20 13:21:18 -0500

    docs

--- docs.sh
+++ docs.sh
@@ -3,6 +3,7 @@ rm -rf ./docs/*.html
 sed -i -e  '1,2d' ./src/amps/__init__.py
 pdoc3 --html ./src/amps --output-dir ./docs --force --template-dir ./templates
 mv ./docs/amps/* ./docs
+mv ./docs/* ./mft-labs/documentation/docs
 rm -rf ./docs/amps
 echo 'from erlport.erlang import cast, call\nfrom erlport.erlterms import Atom' | cat - ./src/amps/__init__.py > temp && mv temp ./src/amps/__init__.py
 
