23a5e4c0f4e4eadea35b2c46fe226fae3d218e53
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-05-20 01:27:12 -0500

    docs script

--- docs.sh
+++ docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -i.bak '1,2d' ./src/amps/amps.py
-pdoc3 --html ./src/amps --output-dir ./docs --force
+pdoc3 --html ./src/amps --output-dir ./docs --force --template-dir ./templates
 mv ./docs/amps/* ./docs
 rm -rf ./docs/amps
 echo 'from erlport.erlang import cast, call\nfrom erlport.erlterms import Atom' | cat - ./src/amps/amps.py > temp && mv temp ./src/amps/amps.py
---
+++ templates/logo.mako
@@ -0,0 +1,6 @@
+
+ <header> 
+     <a class="homelink" rel="home" title="amps-py Home" href="https://mft-labs.github.io/amps-py/"> 
+         <img src="default.png" alt=""> pdoc 
+     </a> 
+ </header> 
