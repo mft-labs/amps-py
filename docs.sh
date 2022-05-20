6d85e90eae933cad3723884533a7e814dc528852
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-05-20 12:51:20 -0500

    update workflow

--- docs.sh
+++ docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf ./docs/*.html
-sed -i '1,2d' ./src/amps/__init__.py
+sed -i -e  '1,2d' ./src/amps/__init__.py
 pdoc3 --html ./src/amps --output-dir ./docs --force --template-dir ./templates
 mv ./docs/amps/* ./docs
 rm -rf ./docs/amps
