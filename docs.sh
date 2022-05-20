482473ffad570ff6e4c39c019fa9c13b560ec010
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-05-20 12:15:40 -0500

    updated docs

--- docs.sh
+++ docs.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
-sed -i.bak '1,2d' ./src/amps/amps.py
+rm -rf ./docs/*
+cp black.png ./docs/black.png
+sed -i.bak '1,2d' ./src/amps/__init__.py
 pdoc3 --html ./src/amps --output-dir ./docs --force --template-dir ./templates
 mv ./docs/amps/* ./docs
 rm -rf ./docs/amps
-echo 'from erlport.erlang import cast, call\nfrom erlport.erlterms import Atom' | cat - ./src/amps/amps.py > temp && mv temp ./src/amps/amps.py
-rm ./src/amps/amps.py.bak
+echo 'from erlport.erlang import cast, call\nfrom erlport.erlterms import Atom' | cat - ./src/amps/__init__.py > temp && mv temp ./src/amps/__init__.py
+rm ./src/amps/*.bak
 
